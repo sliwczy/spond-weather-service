2b2b3f67bc02669a9c057afd52c4305f84105ebd
sliwczy <msliwa77@gmail.com>
2025-03-14 13:52:12 +0100

    added handling http statuses;

--- src/main/java/com/spond/WeatherService/service/WeatherService.java
+++ src/main/java/com/spond/WeatherService/service/WeatherService.java
@@ -1,9 +1,15 @@
 package com.spond.WeatherService.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -12,10 +18,27 @@ public class WeatherService {
 
     private final RestTemplate restTemplate;
 
-    public void queryApi() {
+    private static final String API_URL = "https://api.met.no/weatherapi/locationforecast/2.0/compact";
 
-        var apiEndpointUrl = "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.0&lon=60.0";
-        String response = restTemplate.getForObject(apiEndpointUrl, String.class);
-        log.info(response);
+    public Optional<String> getWeatherInfo(double latitude, double longitude, LocalDateTime forecastDateTime) throws JsonProcessingException {
+        //convert event time to the desired forecast
+        //handle status for 404, 403, 500 etc
+        //there still may be null response if there is no forecast for a given day
+
+        var response = queryApi(latitude, longitude);
+
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new HttpServerErrorException(response.getStatusCode());
+        }
+
+        return Optional.of(response.getBody());
+    }
+
+    private ResponseEntity<String> queryApi(double latitude, double longitude) {
+        String url = new StringBuilder().append(API_URL).append("?")
+                .append("lat=").append(latitude)
+                .append("&lon=").append(longitude).toString();
+
+        return restTemplate.getForEntity(url, String.class);
     }
 }
