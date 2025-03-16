63d6456b9ce84d6a5454e7fad09fff6f2f2dc560
sliwczy <msliwa77@gmail.com>
2025-03-16 21:12:55 +0100

    added configuration to allow custom dto classes past java security

--- src/main/java/com/spond/WeatherService/config/QueueConfig.java
+++ src/main/java/com/spond/WeatherService/config/QueueConfig.java
@@ -3,8 +3,12 @@ package com.spond.WeatherService.config;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 
 @Configuration
@@ -25,8 +29,27 @@ public class QueueConfig {
     }
 
     @Bean
-    public SimpleRabbitListenerContainerFactory batchContainerFactory(ConnectionFactory connectionFactory) {
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, SimpleMessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);  // Explicitly setting the converter
+        return template;
+    }
+
+    @Bean
+    public SimpleMessageConverter messageConverter() {
+        var converter = new SimpleMessageConverter();
+        converter.setAllowedListPatterns(List.of(
+                "com.spond.WeatherService.dto.*",
+                "java.util.*",
+                "java.time.*"
+        ));
+        return converter;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory batchContainerFactory(ConnectionFactory connectionFactory, SimpleMessageConverter messageConverter) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setMessageConverter(messageConverter);
         factory.setConnectionFactory(connectionFactory);
         factory.setBatchSize(20);
         factory.setPrefetchCount(20);
